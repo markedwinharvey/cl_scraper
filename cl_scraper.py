1a5006707b04e95711484f5eb3d4f8d1ee552ec7
Mark Harvey <markedwinharvey@gmail.com>
2016-07-26 21:53:36 -0700

    update scraper

--- cl_scraper.py
+++ cl_scraper.py
@@ -11,9 +11,18 @@ from datetime import datetime as dt
 from random import random as rand
 
 cats = {
+	'm4w': 'search/m4w',
 	'free': 'search/zip',
-	'pets': 'search/pet'
+	'w4m': 'search/w4m',
+	'pets': 'search/pet',
+	'cas_w4m': 'search/cas?query=w4m',
+	'cas_m4w': 'search/cas?query=m4w',
+	'cas_m4m': 'search/cas?query=m4m',
+	'cas_w4w': 'search/cas?query=w4w',
+	'm4m': 'search/m4m'
+	
 }
+
 #-------- class declarations --------#
 class nation():
 	def __init__(self,name):
@@ -87,17 +96,38 @@ def scrape(filename, state_name, substate_name, base_url, cat):
 				check_end = total_count - 100
 			else:
 				check_end = total_count - (total_count % 100)
-		
-			url = url+'?s='+str(check_end)
-			n=4+2*int(100*rand())/100.
+			if '?' not in url:
+				url = url+'?s='+str(check_end)
+			else:
+				url_mod = url.split('?')
+				url_start = url_mod[0]
+				url_end = url_mod[1]
+				
+				#url = url_start + '?s='+str(check_end)+'&sort=date&'+url_end 
+				url = url_start + '?s='+str(check_end)+'&'+url_end +'&sort=date'
+				#sometimes the order matters!
+				
+				
+			n=12+4*int(100*rand())/100.
 			time.sleep(n)							#try to avoid the bot blockers
+			
 			page = bs(get_page(url))
 			
 			times = page.find_all('time')
-			time2 = times[-1].get('datetime')
+			try:
+				time2 = times[-1].get('datetime')
+			except:
+				print 'Failed on url:',url
 			
 		else:
-			time2 = times[-1].get('datetime')	
+			#total count on page < 100; need to extract last datetime before "nearby results"
+			entries = page.select('.rows > *')		
+
+			for i in entries:
+				if i.find('time'):
+					time2 = i.find('time').get('datetime')
+				else:
+					break
 			
 		time1 = str(time1).replace(' ','_')
 		time2 = str(time2).replace(' ','_')
@@ -106,30 +136,39 @@ def scrape(filename, state_name, substate_name, base_url, cat):
 		total_count = 0; time2 = 'NA'; time1 = 'NA'
 
 	
-	new_data = ','.join([state_name, substate_name, base_url, str(total_count), time2, time1])
+	new_data = ','.join([state_name, substate_name, url, str(total_count), time2, time1])
 	with open('data/'+filename,'a') as f:			#append each entry to data file
 		f.write(new_data+'\n')
 
 
 def main():
 
-	cat = 'pets'
+	cat = 'cas_w4w'
 	
 	r = requests.get('https://www.craigslist.org/about/sites')
 	page=r.text
-
+	#with open('r','r') as f:
+	#	page = f.read()
+		
+	time.sleep(4)
 	soup = bs(page)
 	
 	usa = make_usa(soup)
 	
+	'''filename'''
 	filename = str(dt.now()).split('.')[0].replace(' ','_')+'_'+cat
+	'''filename'''
+	
 	index = 1
 	for state in sorted(usa.states):
 		for substate in sorted(usa.states[state].substates):
 			sub = usa.states[state].substates[substate]
 			print sub.name,sub.url
 			
+			#'''index'''
 			if index > 0:						#for resuming collection when script fails from bad site data
+			#'''index'''	
+			
 				scrape(
 					filename=filename,
 					state_name=state,
@@ -138,7 +177,7 @@ def main():
 					cat=cat
 				)
 		
-				n=10+3*int(100*rand())/100.		
+				n=12+3*int(100*rand())/100.		
 				time.sleep(n)					#try to avoid the bot blockers
 			
 			index+=1
